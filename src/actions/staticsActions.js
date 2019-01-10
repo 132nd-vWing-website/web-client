8201885a3a06d45dc7f7e9d6271ff670e393bd82
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-10 22:40:44 +0100

    Now uses correct API root

--- src/actions/staticsActions.js
+++ src/actions/staticsActions.js
@@ -1,12 +1,14 @@
 /* eslint import/prefer-default-export: off */
 import axios from 'axios';
 
+import API_ROOT from './api-config';
+
 import { GET_ALL_FRONTPAGESLIDERS, GET_ERRORS } from './types';
 
 // Get all Frontpage Slider Documents from Sanity
 export const getSliders = () => (dispatch) => {
   axios
-    .get('/statics/frontpagesliders')
+    .get(`${API_ROOT}/statics/frontpagesliders`)
     .then((res) => dispatch({ type: GET_ALL_FRONTPAGESLIDERS, payload: res.data }))
     .catch((err) => dispatch({ type: GET_ERRORS, payload: err.response.data }));
 };
