50bd3abab925ebaa46b0db6883d42937009bb024
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-10 20:33:38 +0100

    Connected to redux store

--- src/components/HeaderCarousel/HeaderCarousel.jsx
+++ src/components/HeaderCarousel/HeaderCarousel.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { Carousel } from 'antd';
 
+import { getSliders } from '../../actions/staticsActions';
+
 import './style.css';
 
-export default class HeaderCarousel extends Component {
+class HeaderCarousel extends Component {
+  state = {
+    slides: [],
+  };
+
+  componentDidMount() {
+    const { fetchSlides } = this.props;
+    fetchSlides();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { slides } = this.state;
+    if (slides !== nextProps.slides) return true;
+    return false;
+  }
+
+  sortSlides = (slides) => {
+    slides.sort((a, b) => {
+      if (a.index > b.index) return -1;
+      if (a.index < b.index) return 1;
+      return 0;
+    });
+  };
+
   render() {
     return (
       <Carousel effect='fade' autoplay>
@@ -24,3 +51,17 @@ export default class HeaderCarousel extends Component {
     );
   }
 }
+
+HeaderCarousel.propTypes = {
+  fetchSlides: PropTypes.func.isRequired,
+  slides: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  slides: state.statics.frontpageSliders,
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchSlides: getSliders },
+)(HeaderCarousel);
