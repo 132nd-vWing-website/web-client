e8300fc73a80eaeba825f074c0e0c726e7a5d46f
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-10 20:33:28 +0100

    added eslinter exception for default export

--- src/actions/staticsActions.js
+++ src/actions/staticsActions.js
@@ -1,13 +1,12 @@
+/* eslint import/prefer-default-export: off */
 import axios from 'axios';
 
 import { GET_ALL_FRONTPAGESLIDERS, GET_ERRORS } from './types';
 
 // Get all Frontpage Slider Documents from Sanity
-const getSliders = () => (dispatch) => {
+export const getSliders = () => (dispatch) => {
   axios
     .get('/statics/frontpagesliders')
     .then((res) => dispatch({ type: GET_ALL_FRONTPAGESLIDERS, payload: res.data }))
     .catch((err) => dispatch({ type: GET_ERRORS, payload: err.response.data }));
 };
-
-export default getSliders();
