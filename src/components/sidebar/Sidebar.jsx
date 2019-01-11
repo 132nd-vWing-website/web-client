a94e4c702b997287a35812763f3c02c945ff2a4a
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-11 23:43:32 +0100

    formatting

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -100,7 +100,14 @@ const Sidebar = (props) => {
 
   return (
     <Sider breakpoint='lg' collapsedWidth='0' className='sider-main'>
-      <div className='logo' style={{ margin: '1em 0 0 0', background: `url(${Logo}) 0 0 / cover no-repeat`, minHeight: '200px' }} />
+      <div
+        className='logo'
+        style={{
+          margin: '1em 0 0 0',
+          background: `url(${Logo}) 0 0 / cover no-repeat`,
+          minHeight: '200px',
+        }}
+      />
       {isAuthenticated ? authLinks : guestLinks}
     </Sider>
   );
