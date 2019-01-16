60e816d9635155fa54a321a3fabd9beda7e2e7c3
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-01-16 14:19:39 +0100

    Added prop validation

--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Card, Row, Col } from 'antd';
 
-const Event = ({ match }) => {
-  console.log('EVENT!');
-  return (
-    <Card title='Event'>
-      <Row>
-        <Col className='gutter-row' span={24} md={24}>
-          <h3>
-            Event:
-            {match.params.id}
-          </h3>
-        </Col>
-      </Row>
-    </Card>
-  );
+const Event = ({ match }) => (
+  <Card title='Event'>
+    <Row>
+      <Col className='gutter-row' span={24} md={24}>
+        <h3>
+          Event:
+          {match.params.id}
+        </h3>
+      </Col>
+    </Row>
+  </Card>
+);
+
+Event.propTypes = {
+  match: PropTypes.object.isRequired,
 };
 
 export default Event;
