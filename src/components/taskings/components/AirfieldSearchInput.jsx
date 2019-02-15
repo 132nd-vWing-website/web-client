8b3272a79403921a9d5c97d66f02e1fd258de89d
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-15 18:52:56 +0100

    Refactored to use React Hooks

--- src/components/taskings/components/AirfieldSearchInput.jsx
+++ src/components/taskings/components/AirfieldSearchInput.jsx
@@ -1,105 +1,77 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import SearchInput from './SearchInput';
 
-export default class AirfieldSearchInput extends Component {
-  state = {
-    airfields: null,
-  };
+function AirfieldSearchInput(props) {
+  const { airfields, name, style, onChange, options } = props;
 
-  componentDidMount() {
-    const { airfields } = this.props;
-    this.setState({ airfields });
-  }
+  // Create airfield options from the list of airfields available
+  const [airfieldOptions, setAirfieldOptions] = useState([]);
+  useEffect(
+    () => {
+      setAirfieldOptions(
+        options.map((airfield) => ({
+          key: `${airfield.airfield_id}`,
+          label: `${airfield.af_icao} (RWY ${airfield.af_rwy}) - ${airfield.af_name}`,
+          value: airfield.airfield_id,
+        })),
+      );
+    },
+    [options],
+  );
 
-  shouldComponentUpdate(nextProps, nextState) {
-    const { airfields, airfieldOptions } = this.state;
-    const { options } = this.props;
-    if (airfields !== nextState.airfields) {
-      return true;
-    }
-    if (airfieldOptions !== nextState.airfieldOptions) {
-      return true;
-    }
-    if (options !== nextProps.options) {
-      return true;
-    }
+  // Match the current selected airfield to the airfieldOption object
+  const [value, setValue] = useState(null);
+  useEffect(
+    () => {
+      // Find the stored airfield...
+      const airfield = airfields.find((field) => field.name === name);
+      // ...match that airfield to our options, so that we can display the correct data
+      const newValue = airfieldOptions.find((option) => option.key === airfield.id.toString());
+      setValue(newValue);
+    },
+    [airfieldOptions],
+  );
 
-    return null;
-  }
+  // Return data to parent component when a new airfield is selected
+  const [result, setResult] = useState(null);
+  useEffect(
+    () => {
+      if (result) {
+        const index = airfields.findIndex((field) => field.name === result.name);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { airfields } = this.state;
-    const { onChange, options } = this.props;
+        const data = options.find((option) => option.airfield_id === result.value);
+        const updatedAirfield = {
+          id: result.value,
+          icao: data.af_icao,
+          tcn: data.af_tcn,
+          gnd: data.af_gnd,
+          twr: data.af_twr,
+          elev: data.af_elev,
+          rwy: data.af_rwy,
+          ils: data.af_ils,
+          ctrl: data.af_ctrl,
+          len: data.af_rwy_length,
+          lat: data.af_lat,
+          lon: data.af_lon,
+        };
 
-    if (airfields !== prevState.airfields) {
-      onChange({ name: 'airfields', value: airfields });
-    }
+        airfields[index] = Object.assign({}, airfields[index], updatedAirfield);
+        onChange({ name: 'airfields', value: airfields });
+      }
+    },
+    [result],
+  );
 
-    if (options !== prevProps.options) {
-      const airfieldOptions = options.map((airfield) => ({
-        key: `${airfield.airfield_id}`,
-        label: `${airfield.af_icao} (RWY ${airfield.af_rwy}) - ${airfield.af_name}`,
-        value: airfield.airfield_id,
-      }));
-
-      this.setState(() => ({
-        airfieldOptions: [...airfieldOptions],
-      }));
-    }
-  }
-
-  handleChange = ({ name, value }) => {
-    const { airfields } = this.state;
-    const { options } = this.props;
-    const index = airfields.findIndex((field) => field.name === name);
-
-    /* AIRFIELD DATA LOOKUP */
-    const data = options.find((option) => option.airfield_id === value);
-    const updatedAirfield = {
-      id: value,
-      icao: data.af_icao,
-      tcn: data.af_tcn,
-      gnd: data.af_gnd,
-      twr: data.af_twr,
-      elev: data.af_elev,
-      rwy: data.af_rwy,
-      ils: data.af_ils,
-      ctrl: data.af_ctrl,
-      len: data.af_rwy_length,
-      lat: data.af_lat,
-      lon: data.af_lon,
-    };
-
-    airfields[index] = Object.assign({}, airfields[index], updatedAirfield);
-
-    this.setState(() => ({
-      airfields: [...airfields],
-    }));
-  };
-
-  render() {
-    const { name, style } = this.props;
-    const { airfields, airfieldOptions } = this.state;
-
-    if (!airfields) return <p>Loading airfields...</p>;
-    if (!airfieldOptions) return <p>Loading options...</p>;
-
-    // Find the stored airfield...
-    const airfield = airfields.find((field) => field.name === name);
-    // ...match that airfield to our options, so that we can display the correct data
-    const value = airfieldOptions.find((option) => option.key === airfield.id.toString());
-
-    return (
-      <SearchInput
-        value={value.label ? value.label : ''}
-        name={name}
-        onChange={this.handleChange}
-        data={airfieldOptions}
-        style={style}
-      />
-    );
-  }
+  return (
+    <SearchInput
+      value={value ? value.label : ''}
+      name={name}
+      onChange={setResult}
+      data={airfieldOptions}
+      style={style}
+    />
+  );
 }
 
 AirfieldSearchInput.propTypes = {
@@ -113,3 +85,5 @@ AirfieldSearchInput.propTypes = {
 AirfieldSearchInput.defaultProps = {
   style: {},
 };
+
+export default AirfieldSearchInput;
