9cb65ad554c9f79f5b4d0b296183cc4602d157a4
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-15 14:18:50 +0100

    Refactored to use React Hooks

--- src/components/taskings/components/SearchInput.jsx
+++ src/components/taskings/components/SearchInput.jsx
@@ -1,70 +1,52 @@
 import { Select } from 'antd';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 /**
  * @namespace SearchInput
  * @returns {object} { name, value }
  */
-export default class SearchInput extends React.Component {
-  state = {
-    data: [],
-    value: null,
-  };
+function SearchInput(props) {
+  const { name, onChange, style, placeholder, data, value } = props;
 
-  componentDidMount() {
-    const { data } = this.props;
-    this.setState({ data });
-  }
+  const [result, setResult] = useState(null);
+  useEffect(
+    () => {
+      if (result) onChange({ name, value: result });
+    },
+    [result],
+  );
 
-  componentDidUpdate(prevProps, prevState) {
-    const { value } = this.state;
-    const { name, onChange, data } = this.props;
+  const [options, setOptions] = useState(null);
+  useEffect(
+    () => {
+      setOptions(
+        data.map((d) => (
+          <Select.Option key={d.key} value={d.value}>
+            {d.label}
+          </Select.Option>
+        )),
+      );
+    },
+    [data],
+  );
 
-    if (value !== prevState.value) {
-      onChange({ name, value });
-    }
-
-    if (data !== prevProps.data) {
-      this.setState(() => ({
-        data: [...data],
-      }));
-    }
-  }
-
-  handleChange = (value) => {
-    this.setState(() => ({
-      value,
-    }));
-  };
-
-  render() {
-    const { value, style, placeholder } = this.props;
-    const { data } = this.state;
-
-    const options = data.map((d) => (
-      <Select.Option key={d.key} value={d.value}>
-        {d.label}
-      </Select.Option>
-    ));
-
-    return (
-      <Select
-        showSearch
-        value={value}
-        placeholder={placeholder}
-        style={style}
-        onChange={this.handleChange}
-        filterOption={(input, option) =>
-          option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-        }
-        showArrow={false}
-        defaultActiveFirstOption={false}
-        notFoundContent={null}>
-        {options}
-      </Select>
-    );
-  }
+  return (
+    <Select
+      showSearch
+      value={result || value}
+      placeholder={placeholder}
+      style={style}
+      onChange={(input) => setResult(input)}
+      filterOption={(input, option) =>
+        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      }
+      showArrow={false}
+      defaultActiveFirstOption={false}
+      notFoundContent={null}>
+      {options}
+    </Select>
+  );
 }
 
 SearchInput.propTypes = {
@@ -81,3 +63,5 @@ SearchInput.defaultProps = {
   value: '',
   placeholder: 'Type to search...',
 };
+
+export default SearchInput;
