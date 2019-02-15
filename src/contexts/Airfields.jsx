3290e5b826f4686807ff6d8670478511dbda5c55
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-15 20:51:05 +0100

    Context provider for Airfields in db

---
+++ src/contexts/Airfields.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import API_ROOT from './api-config';
+
+const AirfieldsContext = React.createContext({
+  airfields: [],
+  setAirfields: () => null,
+});
+
+export const AirfieldsConsumer = AirfieldsContext.Consumer;
+
+export default function AirfieldsProvider(props) {
+  const { children } = props;
+
+  const [airfields, setAirfields] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${API_ROOT}/airfields`)
+      .then((res) => setAirfields(res.data))
+      .catch(() => setAirfields([]));
+  }, []);
+
+  return (
+    <AirfieldsContext.Provider value={{ airfields, setAirfields }}>
+      {children}
+    </AirfieldsContext.Provider>
+  );
+}
+
+AirfieldsProvider.propTypes = {
+  children: PropTypes.object,
+};
+
+AirfieldsProvider.defaultProps = {
+  children: {},
+};
