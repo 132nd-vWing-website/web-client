3c22ac35a77368724943e130491896ac8296b137
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-17 11:37:33 +0100

    Renamed and fixed a buf

--- src/utils/modCSS.js
+++ src/utils/modCSS.js
@@ -1,17 +1,20 @@
+/* eslint no-param-reassign: 0 */
 /**
  * This function is given a css-selector (see css documentation) and returns the CSS rule that matches
  * @param {string} selector - CSS Selector to use for lookup
+ * @param {string} prop - Name of style property to modify
+ * @param {string} value - New value of style
+ *
  */
-export default function cssLookup(selector) {
+export default function modCSS(selector, prop, value) {
   const sheets = document.styleSheets;
 
   Object.values(sheets).forEach((sheet) => {
     Object.values(sheet.cssRules).forEach((rule) => {
       if (rule.selectorText === selector) {
-        return rule.style;
+        rule.style[prop] = `"${value}"`;
       }
       return null;
     });
   });
-  return null;
 }
