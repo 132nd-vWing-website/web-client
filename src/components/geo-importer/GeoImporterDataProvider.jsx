904040ee38707654db6d8058d686e4f33af31820
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-17 16:26:43 +0100

    Cleaned up imports

--- src/components/geo-importer/GeoImporterDataProvider.jsx
+++ src/components/geo-importer/GeoImporterDataProvider.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 export const GeoImporterDataContext = React.createContext({
   data: null,
