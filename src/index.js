7c9048d2d89cc691f9b571472c8ab37b66d6b1e6
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-20 23:45:11 +0100

    removed lazy loading of App

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 /* eslint react/jsx-filename-extension: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
-const App = React.lazy(() => import('./App'));
-const LoadingComponent = <div>Loading...</div>;
+// const App = React.lazy(() => import('./App'));
+// const LoadingComponent = (
+//   <div className='loading-screen'>
+//     <h2>Welcome to the 132nd Virtual Wing</h2>
+//     <p>Loading...</p>
+//   </div>
+// );
 
 /** Main app is just a wrapper to allow lazy-loading of the whole website for slower connections */
-function Main() {
-  return (
-    <React.Suspense fallback={LoadingComponent}>
-      <App />
-    </React.Suspense>
-  );
-}
-
-// const Main = Loadable({
-//   loader: () => import('./App'),
-//   loading: LoadingComponent,
-// });
+// function Main() {
+//   return (
+//     <React.Suspense fallback={LoadingComponent}>
+//       <App />
+//     </React.Suspense>
+//   );
+// }
 
 // function Main() {
 //   const [content, setContent] = useState(null);
-//   useEffect(() => import('./App.jsx').then((module) => setContent(module.default)), []);
+//   useEffect(
+//     () =>
+//       import('./App.jsx').then((module) => {
+//         setContent(module.default);
+//         return null;
+//       }),
+//     [],
+//   );
 
-//   return content || <div>Loading...</div>;
+//   return content || LoadingComponent;
 // }
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+// ReactDOM.render(<Main />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
