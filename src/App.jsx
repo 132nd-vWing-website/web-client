22b63f071002b9f581167a8d7bfb7fef54832bd1
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-20 08:35:48 +0100

    Fixed mobile-sider-tab hidden behind cards

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,10 @@ import store from './store';
 // Utils
 import setAuthToken from './utils/setAuthToken';
 
-// const LoadingComponent = <div>Loading...</div>;
+// Lazy Loading
+const LoadingComponent = <div>Loading...</div>;
+
+// const Tasking = React.lazy(() => import('./components/taskings/Tasking'));
 // const HeaderCarousel = Loadable({
 //   loader: () => import('./components/headercarousel/HeaderCarousel'),
 //   loading: () => LoadingComponent,
@@ -91,42 +94,44 @@ function App() {
   console.log('App reloaded at ', new Date());
 
   return (
-    <Provider store={store}>
-      <MissionDataProvider>
-        <Router>
-          <Layout style={{ minHeight: '100vh' }}>
-            <Sidebar />
-            <Layout style={{ background: '#272727' }}>
-              <HeaderComponent />
-              <Content style={{ margin: '1em 1em 0' }}>
-                <Switch>
-                  <Route exact path='/' component={Landing} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
-                  <Route path='/events' component={Events} />
-                  <Route path='/taskings' component={Tasking} />
-                  <Route path='/pdf' component={MDCDemo} />
-                  <Route component={Landing} />
-                </Switch>
-                <Switch>
-                  <PrivateRoute exact path='/dashboard' component={ProfileDashboard} />
-                </Switch>
-                <Switch>
-                  <PrivateRoute
-                    exact
-                    path='/create-profile'
-                    component={<div>CREATE A PROFILE</div>}
-                  />
-                </Switch>
-              </Content>
-              <Footer style={{ textAlign: 'center', background: '#272727', color: '#aaa' }}>
-                132nd Virtual Wing ©2019
-              </Footer>
+    <React.Suspense fallback={LoadingComponent}>
+      <Provider store={store}>
+        <MissionDataProvider>
+          <Router>
+            <Layout style={{ minHeight: '100vh' }}>
+              <Sidebar />
+              <Layout style={{ background: '#272727' }}>
+                {/* <HeaderComponent /> */}
+                <Content style={{ margin: '1em 1em 0' }}>
+                  <Switch>
+                    <Route exact path='/' component={Landing} />
+                    <Route exact path='/register' component={Register} />
+                    <Route exact path='/login' component={Login} />
+                    <Route path='/events' component={Events} />
+                    <Route path='/taskings' component={Tasking} />
+                    <Route path='/pdf' component={MDCDemo} />
+                    <Route component={Landing} />
+                  </Switch>
+                  <Switch>
+                    <PrivateRoute exact path='/dashboard' component={ProfileDashboard} />
+                  </Switch>
+                  <Switch>
+                    <PrivateRoute
+                      exact
+                      path='/create-profile'
+                      component={<div>CREATE A PROFILE</div>}
+                    />
+                  </Switch>
+                </Content>
+                <Footer style={{ textAlign: 'center', background: '#272727', color: '#aaa' }}>
+                  132nd Virtual Wing ©2019
+                </Footer>
+              </Layout>
             </Layout>
-          </Layout>
-        </Router>
-      </MissionDataProvider>
-    </Provider>
+          </Router>
+        </MissionDataProvider>
+      </Provider>
+    </React.Suspense>
   );
 }
 
@@ -135,54 +140,8 @@ export default App;
 /**
  * Header Component
  */
-const HeaderComponent = () => (
-  // <Header style={{ background: '#fff', padding: 0, minHeight: '350px', margin: '1em 1em 0' }}>
-  //   <HeaderCarousel />
-  // </Header>
-  <div />
-);
-
-// function App() {
-//   return (
-//     <Router>
-//       <Layout className='layout'>
-//         <Header>
-//           <div className='brand-logo'>StudioDash</div>
-//           <Menu
-//             theme='dark'
-//             mode='horizontal'
-//             defaultSelectedKeys={['1']}
-//             style={{ lineHeight: '64px' }}>
-//             <Menu.Item key='1'>
-//               <Icon type='dashboard' theme='filled' />
-//               <span>Dashboard</span>
-//               <Link to='/' />
-//             </Menu.Item>
-//             <Menu.Item key='2'>
-//               <Icon type='profile' theme='filled' />
-//               <span>Navigator</span>
-//               <Link to='/navigator' />
-//             </Menu.Item>
-//           </Menu>
-//         </Header>
-//         <Content style={{ padding: '0 50px ' }} xs={24}>
-//           <div style={{ padding: 24, minHeight: '80vh' }}>
-//             <StudioClient
-//               studio={studio}
-//               studioUrl={Config.applicationURL}
-//               render={(client) => (
-//                 <React.Fragment>
-//                   <Route exact path='/dashboard' render={() => <DashboardTab client={client} />} />
-//                   <Route exact path='/' render={() => <NavigatorTab client={client} />} />
-//                 </React.Fragment>
-//               )}
-//             />
-//           </div>
-//         </Content>
-//         <Footer style={{ textAlign: 'center' }}>
-//           StudioDash ©2019 Created by Jens Kristian Hoel
-//         </Footer>
-//       </Layout>
-//     </Router>
-//   );
-// }
+// const HeaderComponent = () => (
+//   <Header style={{ background: '#fff', padding: 0, minHeight: '350px', margin: '1em 1em 0' }}>
+//     <HeaderCarousel />
+//   </Header>
+// );
