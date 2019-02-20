66faf9a2818e11a00a0e7f842d4f381ad308e381
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2019-02-20 17:04:15 +0100

    Cleaned up, and enabled lazy loading of App

--- src/index.js
+++ src/index.js
@@ -1,38 +1,31 @@
 /* eslint react/jsx-filename-extension: 0 */
-// import Loadable from 'react-loadable';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 // import registerServiceWorker from './registerServiceWorker';
 
-// import App from './App';
-
-/* eslint import/no-extraneous-dependencies: 0 */
-// import 'core-js/modules/es6.promise';
-// import 'core-js/modules/es6.array.iterator';
-
-// const App = React.lazy(() => import('./App'));
-// const LoadingComponent = <div>Loading...</div>;
+const App = React.lazy(() => import('./App'));
+const LoadingComponent = <div>Loading...</div>;
 
 /** Main app is just a wrapper to allow lazy-loading of the whole website for slower connections */
-// function Main() {
-//   return (
-//     <React.Suspense fallback={LoadingComponent}>
-//       <App />
-//     </React.Suspense>
-//   );
-// }
+function Main() {
+  return (
+    <React.Suspense fallback={LoadingComponent}>
+      <App />
+    </React.Suspense>
+  );
+}
 
 // const Main = Loadable({
 //   loader: () => import('./App'),
 //   loading: LoadingComponent,
 // });
 
-function Main() {
-  const [content, setContent] = useState(null);
-  useEffect(() => import('./App.jsx').then((module) => setContent(module.default)), []);
+// function Main() {
+//   const [content, setContent] = useState(null);
+//   useEffect(() => import('./App.jsx').then((module) => setContent(module.default)), []);
 
-  return content || <div>Loading...</div>;
-}
+//   return content || <div>Loading...</div>;
+// }
 
 ReactDOM.render(<Main />, document.getElementById('root'));
 // registerServiceWorker();
