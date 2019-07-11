68f9519c055c243ee62ce199b0992aa3ea0d3d19
Jens Kristian Hoel <github@hoelweb.com>
2019-07-12 00:32:49 +0200

    FIX: Selected value now gets returned correctly

--- src/components/ui/Autocomplete/AirfieldSelector.jsx
+++ src/components/ui/Autocomplete/AirfieldSelector.jsx
@@ -5,81 +5,84 @@ import Autosuggest from 'react-autosuggest';
 import './theme.css';
 
 export default function AirfieldSelector({ name, label, id, placeholder, onChange, data }) {
-    const [value, setValue] = React.useState('')
-    const [suggestions, setSuggestions] = React.useState([])
-    React.useEffect(() => {
-        let e = {
-            target: {
-                name: name,
-                value: value
-            }
-        }
-        onChange(e)
-    }, [value])
+  const [value, setValue] = React.useState('');
+  const [suggestions, setSuggestions] = React.useState([]);
+  React.useEffect(() => {
+    const e = {
+      target: {
+        name,
+        value,
+      },
+    };
+    onChange(e);
+  }, [value]);
 
+  // Function for calculating suggestion for any given input value
+  const getSuggestions = (input) => {
+    const inputValue = input.trim().toLowerCase();
+    const inputLength = inputValue.length;
 
-    // Function for calculating suggestion for any given input value
-    const getSuggestions = (value) => {
-        const inputValue = value.trim().toLowerCase();
-        const inputLength = inputValue.length;
+    return inputLength === 0
+      ? []
+      : data.filter((item) => item.af_icao.toLowerCase().slice(0, inputLength) === inputValue);
+  };
 
-        return inputLength === 0 ? [] : data.filter(item =>
-            item.af_icao.toLowerCase().slice(0, inputLength) === inputValue
-        );
-    }
+  // Autosuggest needs to populate the input based on the clicked suggestion
+  const getSuggestionValue = (suggestion) => suggestion.af_icao;
 
-    // Autosuggest needs to populate the input based on the clicked suggestion
-    const getSuggestionValue = (suggestion) => suggestion.af_icao;
+  // A function for declaring how each suggestion should be presented
+  const renderSuggestion = (suggestion) => {
+    const str = `${suggestion.af_icao} - ${suggestion.af_name} RWY${suggestion.af_rwy}`;
+    return <div>{str}</div>;
+  };
 
-    // A function for declaring how each suggestion should be presented
-    const renderSuggestion = (suggestion) => (
-        <div>{suggestion.af_icao} - {suggestion.af_name} RWY{suggestion.af_rwy} </div>
-    )
+  // Handle value updates
+  const handleChange = (event, { newValue }) => {
+    setValue(newValue);
+  };
 
-    // Handle value updates
-    const handleChange = (event, { newValue }) => {
-        setValue(newValue)
-    }
+  // Update suggestions as we get input
+  const onSuggestionsFetchRequested = ({ value }) => {
+    setSuggestions(getSuggestions(value));
+  };
 
-    // Update suggestions as we get input
-    const onSuggestionsFetchRequested = ({ value }) => {
-        setSuggestions(getSuggestions(value));
-    }
+  // Function to clear suggestions
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
 
-    // Function to clear suggestions
-    const onSuggestionsClearRequested = () => {
-        setSuggestions([])
-    }
+  // Props to pass down to the input field
+  const inputProps = {
+    placeholder,
+    value,
+    onChange: handleChange,
+  };
 
-    // Props to pass down to the input field
-    const inputProps = {
-        placeholder, value, onChange: handleChange
-    }
-
-    return (
-        <Autosuggest
-            suggestions={suggestions}
-            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-            onSuggestionsClearRequested={onSuggestionsClearRequested}
-            getSuggestionValue={getSuggestionValue}
-            renderSuggestion={renderSuggestion}
-            inputProps={inputProps}
-        />
-    )
+  return (
+    <Autosuggest
+      suggestions={suggestions}
+      onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+      onSuggestionsClearRequested={onSuggestionsClearRequested}
+      getSuggestionValue={getSuggestionValue}
+      renderSuggestion={renderSuggestion}
+      inputProps={inputProps}
+    />
+  );
 }
 
 AirfieldSelector.propTypes = {
-    name: PropTypes.string,
-    label: PropTypes.string,
-    id: PropTypes.string,
-    placeholder: PropTypes.string,
-    onChange: PropTypes.func,
+  name: PropTypes.string,
+  label: PropTypes.string,
+  id: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+  data: PropTypes.array.isRequired,
 };
 
 AirfieldSelector.defaultProps = {
-    name: Date.now().toString(),
-    label: null,
-    id: Date.now().toString(),
-    placeholder: null,
-    onChange: () => null,
+  name: Date.now().toString(),
+  label: null,
+  id: Date.now().toString(),
+  placeholder: null,
+  onChange: () => null,
 };
