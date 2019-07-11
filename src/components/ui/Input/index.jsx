dabcac0021ac00b6e5fa8f782a0750d7a1a25a85
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-07-11 10:56:36 +0200

    ADD: <Input /> added

---
+++ src/components/ui/Input/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Grid, GridItem } from 'styled-grid-component';
+import PropTypes from 'prop-types';
+
+const InputLabel = styled.label`
+    font-weight: 600;
+    float:right;
+`;
+
+const InputField = styled.input`
+    width: 100%;
+    margin-bottom: 2px;
+`;
+
+export default function Input({ label, id, placeholder }) {
+    return (
+        <Grid
+            width="100%"
+            templateColumns="repeat(6, 1fr)"
+            gap="10px"
+        >
+            <GridItem column="1 / 2" row="1">
+                <InputLabel htmlFor={`input-${id}`}>
+                    {label}
+                </InputLabel>
+            </GridItem>
+            <GridItem column="2 / 7" row="1">
+                <InputField id={`input-${id}`} placeholder={placeholder} />
+            </GridItem>
+        </Grid>
+    )
+}
+
+Input.propTypes = {
+    label: PropTypes.string,
+    id: PropTypes.string,
+    placeholder: PropTypes.string,
+
+};
+
+Input.defaultProps = {
+    label: 'Input:',
+    id: Date.now(),
+    placeholder: null,
+};
