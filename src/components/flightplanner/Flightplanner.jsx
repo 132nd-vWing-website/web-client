10049ee892bb711a3baeac198b39e16bea873695
jens.kristian.hoel <jens.kristian.hoel@seaonics.com>
2019-07-11 09:51:05 +0200

    CHANGE: Flightplanner now uses new Page component

--- src/components/flightplanner/Flightplanner.jsx
+++ src/components/flightplanner/Flightplanner.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { FlightplanContext } from './FlightplanContext';
 import Collapsible, { CollapsibleGroup } from '../ui/Collapsible';
+import Page from '../ui/Page'
 
 export default function Flightplanner({ match }) {
   const { flightplan, setFlightplan, setTaskingId } = useContext(FlightplanContext);
@@ -8,16 +9,18 @@ export default function Flightplanner({ match }) {
   // if (match.params.id) setTaskingId(match.params.id);
 
   return (
-    <CollapsibleGroup>
-      <Collapsible title='Some meaningfull title'>
-        <p>Some Content Here!</p>
-      </Collapsible>
-      <Collapsible title='Some meaningfull title'>
-        <p>Some Content Here!</p>
-      </Collapsible>
-      <Collapsible title='Some meaningfull title'>
-        <p>Some Content Here!</p>
-      </Collapsible>
-    </CollapsibleGroup>
+    <Page title="Flightplanner">
+      <CollapsibleGroup>
+        <Collapsible title='Some meaningfull title'>
+          <p>Some Content Here!</p>
+        </Collapsible>
+        <Collapsible title='Some meaningfull title'>
+          <p>Some Content Here!</p>
+        </Collapsible>
+        <Collapsible title='Some meaningfull title'>
+          <p>Some Content Here!</p>
+        </Collapsible>
+      </CollapsibleGroup>
+    </Page>
   );
 }
