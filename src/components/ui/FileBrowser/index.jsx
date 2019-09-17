012592afc88b9952c7306e4995611bb6a7f67e11
Jens Kristian Hoel <github@hoelweb.com>
2019-09-17 21:44:53 +0200

    Styling now more in line with the Input component

--- src/components/ui/FileBrowser/index.jsx
+++ src/components/ui/FileBrowser/index.jsx
@@ -54,15 +54,15 @@ const Label = styled.label`
   right: 0;
   left: 0;
   z-index: 1;
-  /* height: calc(1.5em + 0.75rem + 2px); */
-  /* padding: 0.375rem 0.75rem; */
   padding-left: 0.375rem;
-  font-weight: 400;
   line-height: 1.5;
   color: #495057;
   background-color: #fff;
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 2px;
+
+  min-height: 25px;
+  font-size: inherit;
 
   display: inline-block;
   margin-bottom: 0.5rem;
@@ -80,15 +80,14 @@ const Label = styled.label`
     bottom: 0;
     z-index: 3;
     display: block;
-    height: calc(1.5em + 0.75rem);
-    padding: 0.375rem 0.75rem;
+    padding: 0 0.375rem;
     line-height: 1.5;
     color: #495057;
     background-color: #e9ecef;
     content: 'Browse';
 
     border-left: inherit;
-    border-radius: 0 0.25rem 0.25rem 0;
+    border-radius: 2px;
   }
 `;
 
